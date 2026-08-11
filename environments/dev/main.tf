adebbafcfda1b7769c79a7a68a1a6a2b25cd76aa
Kenneth Williams <kennepo23@gmail.com>
2026-08-11 05:07:19 -0400

    Added lambda function handlers.

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -442,12 +442,11 @@ resource "aws_iam_role_policy_attachment" "lambda_sqs_attach" {
 
 
 resource "aws_lambda_function" "dev_lambda" {
-  filename         = "lambda_zip"
+  filename         = "lambda.zip"
   function_name    = "sqs_message_processor-dev"
   role             = aws_iam_role.lambda_exec_role.arn
   handler          = "index.handler"
   runtime          = "nodejs18.x"
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   environment {
     variables = {
       SENDER_EMAIL = "blessedbox789@gmail.com" # Este correo DEBE estar verificado en AWS SES
